238512d56b4e7037c1cb85ab7def9c2c7de4a2d9
simon-jk-casey <jesusiwasevil@gmail.com>
2017-04-07 18:20:15 +1200

    Updates

---
+++ index.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import Router from 'sheet-router'
+
+import reducer from './reducer'
+
+// COMPONENTS
+import Signup from './components/signup'
+import Login from './components/main'
+import History from './components/history'
+import Devices from './components/devices'
+import AddDevice from './components/addDevice'
+import DataEntry from './components/dataEntry'
+import Profile from './components/profile'
+import Views from './components/dataMapViewer'
+// SERVICES
+
+const initialState = {
+  route: '/',
+  loginDetails: {},
+  newUser: {},
+  userDetails: {},
+  deviceList: [],
+  newDevice: {},
+  newPredatorData: {},
+  historyToggled: false,
+  historyData: {}
+}
+
+const store = createStore(reducer, initialState)
+const {getState, dispatch, subscribe} = store
+
+const route = Router({default: '404'}, [
+  ['/', (params) => Login],
+  ['/signup', (params) => Signup],
+  ['/profile', (params) => Profile],
+  ['/history', (params) => History],
+  ['/devices', (params) => Devices],
+  ['/addDevice', (params) => AddDevice],
+  ['/dataEntry', (params) => DataEntry],
+  ['/views', (params) => Views]
+])
+
+subscribe(() => {
+  const Component = route(getState().route)
+  render(<Component state={getState()} dispatch={dispatch} />, document.querySelector('main'))
+})
+
+dispatch({type: 'INIT'})
---
+++ reducer.js
@@ -0,0 +1,49 @@
+const clone = require('clone')
+
+function reducer (state, action) {
+  var newState = clone(state)
+  switch (action.type) {
+    case 'INIT':
+      return newState
+    case 'USER_SIGNUP':
+      const newUser = newState.newUser
+      newUser[action.payload.field] = action.payload.value
+      return newState
+    case 'UPDATE_LOGIN_DETAILS':
+      const loginDetails = newState.loginDetails
+      loginDetails[action.payload.field] = action.payload.value
+      return newState
+    case 'CHANGE_ROUTE':
+      newState.route = action.payload
+      return newState
+    case 'USER_DETAILS':
+      newState.userDetails = action.payload
+      return newState
+    case 'DEVICE_LIST':
+      newState.deviceList = action.payload
+      return newState
+    case 'ADD_DEVICE':
+      const newDevice = newState.newDevice
+      newDevice[action.payload.field] = action.payload.value
+      return newState
+    case 'ADD_PREDATOR':
+      const newPredatorData = newState.newPredatorData
+      newPredatorData[action.payload.field] = action.payload.value
+      return newState
+    case 'ENTRY_HISTORY':
+      newState.historyData = action.payload
+      return newState
+    case 'CLEAR_STATE':
+      newState[action.payload] = {}
+      return newState
+    case 'CLEAR_STATE_CHANGE_ROUTE':
+      newState[action.payload.category] = {}
+      newState.route = action.payload.route
+      return newState
+
+    default:
+      return newState
+  }
+}
+
+module.exports = reducer
