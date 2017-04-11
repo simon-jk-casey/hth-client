dfdc602cc28aab11eaeae6b3c16d9d69f23bbff2
simon-jk-casey <jesusiwasevil@gmail.com>
2017-04-11 16:14:23 +1200

    add base styling to main, menu, profile

--- components/profile.js
+++ components/profile.js
@@ -11,16 +11,56 @@ module.exports = ({state, dispatch}) => {
       <div>
         <h3>Welcome back {firstName}!</h3>
       </div>
-      <div>
-        <p>Username: {username}</p>
-        <p>First Name: {firstName}</p>
-        <p>Last Name: {lastName}</p>
-        <p>Street Number: {streetNumber}</p>
-        <p>Street Name: {streetName}</p>
-        <p>Suburb: {suburb}</p>
-        <p>City: {city}</p>
-        <p>Email Address: {email}</p>
-        <p>GPS Coordinates: {gpsCoords}</p>
+      <div className='profSection'>
+        <table>
+          <tbody>
+            <tr>
+              <th className='profHeader col-2 col-m-2'>Username</th>
+              <td className='profData col-9 col-m-9'>{username}</td>
+              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
+            </tr>
+            <tr>
+              <th className='profHeader col-2 col-m-2'>First Name</th>
+              <td className='profData col-9 col-m-9'>{firstName}</td>
+              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
+            </tr>
+            <tr>
+              <th className='profHeader col-2 col-m-2'>Last Name</th>
+              <td className='profData col-9 col-m-9'>{lastName}</td>
+              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
+            </tr>
+            <tr>
+              <th className='profHeader col-2 col-m-2'>Street Number</th>
+              <td className='profData col-9 col-m-9'>{streetNumber}</td>
+              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
+            </tr>
+            <tr>
+              <th className='profHeader col-2 col-m-2'>Street Name</th>
+              <td className='profData col-9 col-m-9'>{streetName}</td>
+              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
+            </tr>
+            <tr>
+              <th className='profHeader col-2 col-m-2'>Suburb</th>
+              <td className='profData col-9 col-m-9'>{suburb}</td>
+              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
+            </tr>
+            <tr>
+              <th className='profHeader col-2 col-m-2'>City</th>
+              <td className='profData col-9 col-m-9'>{city}</td>
+              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
+            </tr>
+            <tr>
+              <th className='profHeader col-2 col-m-2'>Email</th>
+              <td className='profData col-9 col-m-9'>{email}</td>
+              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
+            </tr>
+            <tr>
+              <th className='profHeader col-2 col-m-2'>GPS Coords</th>
+              <td className='profData col-9 col-m-9'>{gpsCoords}</td>
+              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   )
