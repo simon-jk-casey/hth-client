0e9e0fe22a69a72bfa93d51456cd2102b4d0501b
simon-jk-casey <jesusiwasevil@gmail.com>
2017-04-12 14:45:40 +1200

    add css for profile page, fixed menu button width

--- components/profile.js
+++ components/profile.js
@@ -12,55 +12,51 @@ module.exports = ({state, dispatch}) => {
         <h3>Welcome back {firstName}!</h3>
       </div>
       <div className='profSection'>
-        <table>
-          <tbody>
-            <tr>
-              <th className='profHeader col-2 col-m-2'>Username</th>
-              <td className='profData col-9 col-m-9'>{username}</td>
-              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
-            </tr>
-            <tr>
-              <th className='profHeader col-2 col-m-2'>First Name</th>
-              <td className='profData col-9 col-m-9'>{firstName}</td>
-              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
-            </tr>
-            <tr>
-              <th className='profHeader col-2 col-m-2'>Last Name</th>
-              <td className='profData col-9 col-m-9'>{lastName}</td>
-              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
-            </tr>
-            <tr>
-              <th className='profHeader col-2 col-m-2'>Street Number</th>
-              <td className='profData col-9 col-m-9'>{streetNumber}</td>
-              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
-            </tr>
-            <tr>
-              <th className='profHeader col-2 col-m-2'>Street Name</th>
-              <td className='profData col-9 col-m-9'>{streetName}</td>
-              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
-            </tr>
-            <tr>
-              <th className='profHeader col-2 col-m-2'>Suburb</th>
-              <td className='profData col-9 col-m-9'>{suburb}</td>
-              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
-            </tr>
-            <tr>
-              <th className='profHeader col-2 col-m-2'>City</th>
-              <td className='profData col-9 col-m-9'>{city}</td>
-              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
-            </tr>
-            <tr>
-              <th className='profHeader col-2 col-m-2'>Email</th>
-              <td className='profData col-9 col-m-9'>{email}</td>
-              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
-            </tr>
-            <tr>
-              <th className='profHeader col-2 col-m-2'>GPS Coords</th>
-              <td className='profData col-9 col-m-9'>{gpsCoords}</td>
-              <td className='editButton col-1 col-m-1'><button>Edit</button></td>
-            </tr>
-          </tbody>
-        </table>
+        <div className='profRow'>
+          <div className='profHeader col-2 col-m-2'><h4>Username:</h4></div>
+          <div className='profData col-7 col-m-7'>{username}</div>
+          <div className='editButton col-1 col-m-1'><button>Edit</button></div>
+        </div>
+        <div className='profRow'>
+          <div className='profHeader col-2 col-m-2'><h4>First Name:</h4></div>
+          <div className='profData col-7 col-m-7'>{firstName}</div>
+          <div className='editButton col-1 col-m-1'><button>Edit</button></div>
+        </div>
+        <div className='profRow'>
+          <div className='profHeader col-2 col-m-2'><h4>Last Name:</h4></div>
+          <div className='profData col-7 col-m-7'>{lastName}</div>
+          <div className='editButton col-1 col-m-1'><button>Edit</button></div>
+        </div>
+        <div className='profRow'>
+          <div className='profHeader col-2 col-m-2'><h4>Street Number:</h4></div>
+          <div className='profData col-7 col-m-7'>{streetNumber}</div>
+          <div className='editButton col-1 col-m-1'><button>Edit</button></div>
+        </div>
+        <div className='profRow'>
+          <div className='profHeader col-2 col-m-2'><h4>Street Name:</h4></div>
+          <div className='profData col-7 col-m-7'>{streetName}</div>
+          <div className='editButton col-1 col-m-1'><button>Edit</button></div>
+        </div>
+        <div className='profRow'>
+          <div className='profHeader col-2 col-m-2'><h4>Suburb:</h4></div>
+          <div className='profData col-7 col-m-7'>{suburb}</div>
+          <div className='editButton col-1 col-m-1'><button>Edit</button></div>
+        </div>
+        <div className='profRow'>
+          <div className='profHeader col-2 col-m-2'><h4>City:</h4></div>
+          <div className='profData col-7 col-m-7'>{city}</div>
+          <div className='editButton col-1 col-m-1'><button>Edit</button></div>
+        </div>
+        <div className='profRow'>
+          <div className='profHeader col-2 col-m-2'><h4>Email:</h4></div>
+          <div className='profData col-7 col-m-7'>{email}</div>
+          <div className='editButton col-1 col-m-1'><button>Edit</button></div>
+        </div>
+        <div className='profRow'>
+          <div className='profHeader col-2 col-m-2'><h4>GPS Coords:</h4></div>
+          <div className='profData col-7 col-m-7'>{gpsCoords}</div>
+          <div className='editButton col-1 col-m-1'><button>Edit</button></div>
+        </div>
       </div>
     </div>
   )
