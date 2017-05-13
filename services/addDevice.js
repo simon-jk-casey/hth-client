980bed2e2288065d7abe0691240860adc1ffa1c6
simon-jk-casey <jesusiwasevil@gmail.com>
2017-05-13 12:23:26 +1200

    flipped service URLs to deployed

--- services/addDevice.js
+++ services/addDevice.js
@@ -1,9 +1,9 @@
-import request from 'superagent'
+// import request from 'superagent'
 import getDeviceListSvc from '../services/getDevices'
 
 module.exports = (newDevice, dispatch) => {
-  const baseUrl = 'http://localhost:3000' // *dev
-  // const baseUrl = 'https://predata.herokuapp.com' // *deployed
+  // const baseUrl = 'http://localhost:3000' // *dev
+  const baseUrl = 'https://predata.herokuapp.com' // *deployed
   request
   .post(baseUrl + '/api/v1/devices')
   .send(newDevice)
