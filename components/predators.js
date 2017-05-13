181508e5ebc8d4f117975d07cb4f3593637ed0d8
simon-jk-casey <jesusiwasevil@gmail.com>
2017-05-13 13:03:23 +1200

    corrected img src route for deployed (again)

--- components/predators.js
+++ components/predators.js
@@ -16,43 +16,43 @@ module.exports = ({state, dispatch}) => {
     <div className='predatorComponent'>
       <div className='predatorSelectorRow col-12'>
         <div id='Norway Rat' className='predatorCell col-4' onClick={toggleSelected}>
-          <img className='predImg' src='hth-client/images/pests/norwayRat.jpg' />
+          <img className='predImg' src='https://simon-jk-casey.github.io/hth-client/images/pests/norwayRat.jpg' />
             <h4 className='predatorHeading'>Norway Rat</h4>
         </div>
         <div id='Ship Rat' className='predatorCell col-4' onClick={toggleSelected}>
-          <img className='predImg' src='hth-client/images/pests/shipRat.jpg' />
+          <img className='predImg' src='https://simon-jk-casey.github.io/hth-client/images/pests/shipRat.jpg' />
             <h4 className='predatorHeading'>Ship Rat</h4>
         </div>
         <div id='Mouse' className='predatorCell col-4' onClick={toggleSelected}>
-          <img className='predImg' src='hth-client/images/pests/mouse.jpg' />
+          <img className='predImg' src='https://simon-jk-casey.github.io/hth-client/images/pests/mouse.jpg' />
             <h4 className='predatorHeading'>Mouse</h4>
         </div>
       </div>
       <div className='predatorSelectorRow col-12'>
         <div id='Ferret' className='predatorCell col-4' onClick={toggleSelected}>
-          <img className='predImg' src='hth-client/images/pests/ferret.jpg' />
+          <img className='predImg' src='https://simon-jk-casey.github.io/hth-client/images/pests/ferret.jpg' />
             <h4 className='predatorHeading'>Ferret</h4>
         </div>
         <div id='Stoat' className='predatorCell col-4' onClick={toggleSelected}>
-          <img className='predImg' src='hth-client/images/pests/stoat.jpg' />
+          <img className='predImg' src='https://simon-jk-casey.github.io/hth-client/images/pests/stoat.jpg' />
             <h4 className='predatorHeading'>Stoat</h4>
         </div>
         <div id='Weasel' className='predatorCell col-4' onClick={toggleSelected}>
-          <img className='predImg' src='hth-client/images/pests/weasel.jpg' />
+          <img className='predImg' src='https://simon-jk-casey.github.io/hth-client/images/pests/weasel.jpg' />
             <h4 className='predatorHeading'>Weasel</h4>
         </div>
       </div>
       <div className='predatorSelectorRow col-12'>
         <div id='Possum' className='predatorCell col-4' onClick={toggleSelected}>
-          <img className='predImg' src='hth-client/images/pests/possum.jpg' />
+          <img className='predImg' src='https://simon-jk-casey.github.io/hth-client/images/pests/possum.jpg' />
             <h4 className='predatorHeading'>Possum</h4>
         </div>
         <div id='Hedgehog' className='predatorCell col-4' onClick={toggleSelected}>
-          <img className='predImg' src='hth-client/images/pests/hedgehog.jpg' />
+          <img className='predImg' src='https://simon-jk-casey.github.io/hth-client/images/pests/hedgehog.jpg' />
             <h4 className='predatorHeading'>Hedgehog</h4>
         </div>
         <div id='Feral Cat' className='predatorCell col-4' onClick={toggleSelected}>
-          <img className='predImg' src='hth-client/images/pests/feralCat.jpg' />
+          <img className='predImg' src='https://simon-jk-casey.github.io/hth-client/images/pests/feralCat.jpg' />
           <h4 className='predatorHeading'>Feral Cat</h4>
         </div>
       </div>
