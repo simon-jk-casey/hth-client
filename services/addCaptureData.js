b4708eb7789d0c5b475391e09db002c13c8062ac
simon-jk-casey <jesusiwasevil@gmail.com>
2017-05-17 19:51:09 +1200

    flipped baseUrl back to heroku for deployment

--- services/addCaptureData.js
+++ services/addCaptureData.js
@@ -2,8 +2,8 @@ import request from 'superagent'
 import getCaptureDataSvc from '../services/getCaptureData'
 
 module.exports = (predatorData, dispatch) => {
-  const baseUrl = 'http://localhost:3000' // *dev
-  // const baseUrl = 'https://predata.herokuapp.com' // *deployed
+  // const baseUrl = 'http://localhost:3000' // *dev
+  const baseUrl = 'https://predata.herokuapp.com' // *deployed
   request
   .post(baseUrl + '/api/v1/captures')
   .send(predatorData)
